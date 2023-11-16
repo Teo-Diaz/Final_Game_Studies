beb53c35c892301e1d292b28634dad5564a35c00
Kenny <82070420+KennyM14@users.noreply.github.com>
2023-11-16 15:44:51 -0500

    Menú y Puntaje

---
+++ Assets/CambioEscena.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class CambioEscena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambiarEscenas()
+    {
+        SceneManager.LoadScene("Historia"); 
+    }
+
+    public void Salir()
+    {
+        Application.Quit(); 
+    }
+}
